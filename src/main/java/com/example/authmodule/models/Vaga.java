e5b6116fa6ad0b6cba98bece72b116ecf23d29bf
Jrjuniorjr <mjuniorjrjr@gmail.com>
2020-05-31 22:02:10 -0300

    Update Vaga.java

--- src/main/java/com/example/authmodule/models/Vaga.java
+++ src/main/java/com/example/authmodule/models/Vaga.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-@Table(name="tb_paciente")
+@Table(name="tb_vaga")
 public class Vaga {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
