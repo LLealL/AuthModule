4ddaece9edcea6202dacb62742fa7b31a3368fde
LLealL <llealdegoes@gmail.com>
2020-06-08 23:34:07 -0300

    sério?

--- src/main/java/com/example/authmodule/repository/NotificationRepository.java
+++ src/main/java/com/example/authmodule/repository/NotificationRepository.java
@@ -2,7 +2,6 @@ package com.example.authmodule.repository;
 
 import com.example.authmodule.models.Notification;
 import com.example.authmodule.models.User;
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
